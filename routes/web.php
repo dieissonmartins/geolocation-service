6b79b5dfa44641688506a8b111bbd2511bfc7172
dieissonmartins <dieisson.martins.santos@gmail.com>
2021-03-15 15:40:49 -0300

    clear routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use App\Models\Client;
-use Grimzy\LaravelMysqlSpatial\Types\Point;
-use Grimzy\LaravelMysqlSpatial\Types\Polygon;
-use Grimzy\LaravelMysqlSpatial\Types\LineString;
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -28,66 +23,6 @@ $router->group(['prefix' => 'clients', 'middleware' => 'auth'], function() use (
     $router->post('/adverts/local', 'ClientController@showAdvertsLocalization');
 });
 
-$router->post('/adverts/create/store',  'AnnouncementController@store');
-
-
-//test endpoint
-$router->get('geolocation/create', function () {
-    try{
-
-        $loc1 = new Client();
-        $loc1->location = new Point(40.767864, -73.971732);
-        $loc1->name     = "Supermecado BH";
-        $loc1->status   = 1;
-        $loc1->save();
-
-        // Distance from loc1: 44.741406484588
-        $loc2 = new Client();
-        $loc2->location = new Point(40.767664, -73.971271);
-        $loc2->name     = "Mart Minas";
-        $loc2->status   = 1;
-        $loc2->save();
-
-        // Distance from loc1: 870.06424066202
-        $loc3 = new Client();
-        $loc3->location = new Point(40.761434, -73.977619);
-        $loc3->name     = "Xereta supermecados";
-        $loc3->status   = 1;
-        $loc3->save();
-
-        return response()->json([
-            'data' => [
-                'msg' => 'create sucess!'
-            ]
-        ], 200);
-
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 401);
-    }
+$router->group(['prefix' => 'adverts', 'middleware' => 'auth'], function() use ($router) {
+    $router->post('/create/store',  'AnnouncementController@store');
 });
-
-$router->get('geolocation/list', function () {
-    
-    $latitude   = 40.767864;
-    $longitude  = -73.971732;
-    $rayCl      = 30; //raio em milhas
-
-    try{
-        // O método distanceSphere() é da lib instalada, 
-        // você deve informar 3 parâmetros: distanceSphere($geometryColumn, $geometry, $distance); A distância deve ser informada em milhas.
-        $clients = Client::distanceSphere('location', new Point($latitude, $longitude), $rayCl)
-                    ->whereStatus(1) // Aqui é um exemplo de que você pode usar os métodos padrões do seu model junto com os métodos da lib
-                    ->get();
-        
-        return response()->json([
-            'data' => [
-                'advertsTitle' => 'Anúncios no raio de 15 metros',
-                'dataAdverts' =>  $clients
-            ]
-        ], 200);
-
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 401);
-    }
-});
-
