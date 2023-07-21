21a64127e913b3c55a69e6d9845f7337492cf125
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-21 10:36:15 -0300

    clear code

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
 
 /**
- * @property \Grimzy\LaravelMysqlSpatial\Types\Point   $location
+ * @property \Grimzy\LaravelMysqlSpatial\Types\Point $location
  * @property \Grimzy\LaravelMysqlSpatial\Types\Polygon $area
  */
 class Client extends Model
@@ -17,18 +17,18 @@ class Client extends Model
 
     protected $table = 'clients';
 
-    protected $fillable         = ['name','status','location'];
+    protected $fillable = ['name', 'status', 'location'];
 
-    protected $spatialFields    = ['location'];
+    protected $spatialFields = ['location'];
 
     /**
-    * The attributes that should be cast.
-    *
-    * @var array
-    */
-    protected $casts = [ 
-        'created_at'    => 'datetime:d/m/Y H:y:s',
-        'updated_at'    => 'datetime:d/m/Y H:y:s'
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:d/m/Y H:y:s',
+        'updated_at' => 'datetime:d/m/Y H:y:s'
     ];
 
     public function adverts()
